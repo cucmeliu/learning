9ac602e935845c1346ca667f393964918eb6f500
Leo <cucme@163.com>
2017-09-17 21:19:39 +0800

    add knn & python

--- ml/mlinAction/ch2knn/kNN.py
+++ ml/mlinAction/ch2knn/kNN.py
@@ -11,12 +11,53 @@ def classify0(inX, dataSet, labels, k):
 	diffMat = tile(inX, (dataSetSize, 1)) - dataSet
 	sqDiffMat = diffMat**2
 	sqDistances = sqDiffMat.sum(axis=1)
-	distances = sqDistances.argsort()
+	distances = sqDistances**0.5
+	sortedDistIndicies = distances.argsort()
 	classCount = {}
 	for i in range(k):
 		voteIlabel = labels[sortedDistIndicies[i]]
 		classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-	sortedClassCount = sorted(classCount.iteritems(),
+	sortedClassCount = sorted(classCount.items(),
 	 key=operator.itemgetter(1), reverse=True)
-	retrun sortedClassCount[0][0]
+	return sortedClassCount[0][0]
+
+def file2matrix(filename) :
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+def autoNorm(dataSet)
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals - minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape(0)
+    normDataSet = dataSet - tile(minVals, (m, 1))
+    normDataSet = normDataSet / tile(ranges, (m, 1))
+    return normDataSet, ranges, minVals
+
+
+def datingClassTest():
+	hoRatio = 0.10
+	datingDataMat, datingLabels = file2matrix('datingTestSet.txt')
+	normMat, ranges, minVals = autoNorm(datingDataMat)
+	m = normMat.shape[0]
+	numTestVecs = int(m*hoRatio)
+	errorCount = 0.0
+	for 
+
+
+
+
+
 
