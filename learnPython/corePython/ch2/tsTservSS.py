f6949e9b4dd7318c252af595ed4859dc71dcc948
Leo <cucme@163.com>
2017-09-17 22:24:42 +0800

    core python

---
+++ learnPython/corePython/ch2/tsTservSS.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from SocketServer import (TCPServer as TCP, 
+    StreamRequestHandler as SRH)
+from time import ctime
+
+HOST = ''
+PORT = 21567
+ADDR = (HOST, PORT)
+
+class MyRequestHandler(SRH):
+    def handle(self):
+        print '...connect from:', self.client_address
+        self.wfile.write('[%s] %s' % (ctime(),
+            self.rfile.readline()))
+
+tcpServ = TCP(ADDR, MyRequestHandler)
+print 'waiting for connecting...'
+tcpServ.serve_forever()
+
