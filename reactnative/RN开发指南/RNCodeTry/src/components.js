2412562f38cdb4b0bccabf611efcaba2724bfd6d
leo.liu <cucmeliu@gmail.com>
2018-01-02 16:51:01 +0800

    rn & es6

---
+++ reactnative/RN开发指南/RNCodeTry/src/components.js
@@ -0,0 +1,22 @@
+
+
+
+<View>
+  <Text>This is ok!</Text>
+
+  <Image source={require('image!puppies') /}
+</View>
+
+
+
+
+
+
+var styles = StyleSheet.create({
+  bold: {
+    fontWeight: "bold"
+  },
+  italic: {
+    fontStyle: "italic"
+  }
+});
